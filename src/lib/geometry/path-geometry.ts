34859547265cd89f1a4e15e5f88c0f69dc56d89c
Bowen7 <zwxdyx@foxmail.com>
2025-01-13 23:09:46 +0800

    use indexed geometry

--- src/lib/geometry/path-geometry.ts
+++ src/lib/geometry/path-geometry.ts
@@ -11,28 +11,35 @@ export class PathGeometry extends THREE.BufferGeometry {
   constructor(points: Vector2[]) {
     super()
 
-    const verticesArray: number[] = []
-    const uvArray: number[] = []
-    const normalsArray: number[] = []
-
+    const length = points.length
+    const vertices = new Float32Array(length * 3)
+    const uv = new Float32Array(length * 2)
+    const normals = new Float32Array(length * 3)
     const faces = ShapeUtils.triangulateShape(points, [])
+    const indices = new Uint32Array(faces.length * 3)
 
-    const addVertex = (index: number) => {
-      const [x, y] = points[index]
-      verticesArray.push(x, y, 0)
-      uvArray.push(x, y)
-      normalsArray.push(0, 0, 1)
-    }
-
-    faces.forEach((face) => {
-      const [a, b, c] = face
-      addVertex(a)
-      addVertex(b)
-      addVertex(c)
+    points.forEach((point, index) => {
+      const [x, y] = point
+      vertices[index * 3] = x
+      vertices[index * 3 + 1] = y
+      vertices[index * 3 + 2] = 0
+      uv[index * 2] = x
+      uv[index * 2 + 1] = y
+      normals[index * 3] = 0
+      normals[index * 3 + 1] = 0
+      normals[index * 3 + 2] = 1
     })
 
-    this.setAttribute('position', new Float32BufferAttribute(verticesArray, 3))
-    this.setAttribute('uv', new Float32BufferAttribute(uvArray, 2))
-    this.setAttribute('normal', new Float32BufferAttribute(normalsArray, 3))
+    faces.forEach((face, index) => {
+      const [a, b, c] = face
+      indices[index * 3] = a
+      indices[index * 3 + 1] = b
+      indices[index * 3 + 2] = c
+    })
+
+    this.setAttribute('position', new Float32BufferAttribute(vertices, 3))
+    this.setAttribute('uv', new Float32BufferAttribute(uv, 2))
+    this.setAttribute('normal', new Float32BufferAttribute(normals, 3))
+    this.setIndex(new THREE.BufferAttribute(indices, 1))
   }
 }
