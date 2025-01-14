4134f604f6a9c283783216c94a0375df0baa25f9
Bowen7 <zwxdyx@foxmail.com>
2025-01-14 23:12:01 +0800

    extrude path geometry

--- src/lib/geometry/extrude-path-geometry.ts
+++ src/lib/geometry/extrude-path-geometry.ts
@@ -9,8 +9,8 @@ import { PolyGeometry } from './poly-geometry'
 export class ExtrudePathGeometry extends PolyGeometry {
   constructor(points: Vector2[], depth: number) {
     const faces = ShapeUtils.triangulateShape(points, [])
-    const size = points.length * 4
-    const indexSize = faces.length * 6 + points.length * 3
+    const size = points.length * 6
+    const indexSize = faces.length * 6 + points.length
     super({
       size,
       indexSize,
@@ -32,29 +32,26 @@ export class ExtrudePathGeometry extends PolyGeometry {
     buildLid(true)
     buildLid(false)
 
-    const svi = this.vi
     // side faces
     points.forEach((point, index) => {
       const [x, y] = point
       const next = points[index + 1] || points[0]
       const [nx, ny] = next
       const vi = this.vi
+      let u = x
+      let nu = nx
       if (Math.abs(x - nx) < Math.abs(y - ny)) {
-        this.vertex(x, y, depth / 2, y, depth / 2)
-        this.vertex(x, y, -depth / 2, y, depth / 2)
-      } else {
-        this.vertex(x, y, depth / 2, x, depth / 2)
-        this.vertex(x, y, -depth / 2, x, -depth / 2)
+        u = y
+        nu = ny
       }
-      if (index === points.length - 1) {
-        this.face4(vi, vi + 1, svi + 1, svi)
-        const normal = this.calcNormal(vi, vi + 1, svi)
-        this.setNormals(normal, vi, this.vi)
-      } else {
-        // this.face4(vi, vi + 1, vi + 3, vi + 2)
-        // const normal = this.calcNormal(vi, vi + 1, vi + 2)
-        // this.setNormals(normal, vi, this.vi)
-      }
+
+      this.vertex(x, y, depth / 2, u, depth / 2)
+      this.vertex(nx, ny, depth / 2, nu, depth / 2)
+      this.vertex(nx, ny, -depth / 2, nu, -depth / 2)
+      this.vertex(x, y, -depth / 2, u, -depth / 2)
+      this.face4(vi, vi + 1, vi + 2, vi + 3)
+      const normal = this.calcNormal(vi, vi + 1, vi + 2)
+      this.setNormals(normal, vi, this.vi)
     })
 
     this.build()
--- src/modules/canvas/playground.tsx
+++ src/modules/canvas/playground.tsx
@@ -2,14 +2,18 @@ import { useMemo } from 'react'
 import * as THREE from 'three'
 import { useTexture } from '@react-three/drei'
 import { PathGeometry } from '@/lib/geometry/path-geometry'
+import { ExtrudePathGeometry } from '@/lib/geometry/extrude-path-geometry'
 
-const geometry = new PathGeometry(
+const geometry = new ExtrudePathGeometry(
   [
-    new THREE.Vector2(-1, -1),
-    new THREE.Vector2(1, -1),
-    new THREE.Vector2(1, 1),
-    new THREE.Vector2(-1, 1),
+    new THREE.Vector2(-0.5, -0.5),
+    new THREE.Vector2(0.5, -0.5),
+    new THREE.Vector2(1, 0),
+    new THREE.Vector2(0.5, 0.5),
+    new THREE.Vector2(-0.5, 0.5),
+    new THREE.Vector2(-1, 0),
   ],
+  1,
 )
 
 export const Playground = () => {
@@ -24,8 +28,14 @@ export const Playground = () => {
   }, [_texture])
 
   return (
-    <mesh geometry={geometry}>
-      <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
-    </mesh>
+    <>
+      <mesh geometry={geometry}>
+        <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
+      </mesh>
+      {/* <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
+      </mesh> */}
+    </>
   )
 }
